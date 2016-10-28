816ed3a9e82ae9dc2a9878164c75b0137adc838a
Ad_Cargo <adam@cargocreative.co.uk>
2016-10-28 12:23:29 +0100

    no message

--- src/Message/DirectAuthorizeRequest.php
+++ src/Message/DirectAuthorizeRequest.php
@@ -23,7 +23,7 @@ class DirectAuthorizeRequest extends AbstractRequest
         $data['Amount'] = $this->getAmount();
         $data['Currency'] = $this->getCurrency();
         $data['VendorTxCode'] = $this->getVendorData();
-        $data['VendorData'] = $this->getTransactionId();
+        $data['VendorData'] = $this->getVendorData();
         $data['ClientIPAddress'] = $this->getClientIp();
         $data['ApplyAVSCV2'] = $this->getApplyAVSCV2() ?: 0;
         $data['Apply3DSecure'] = $this->getApply3DSecure() ?: 0;
